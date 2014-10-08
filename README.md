419be95f52eabebe4268bab389efd669da315c50
Dooley <nsewolo@gmail.com>
2014-10-07 22:15:18 -0400

    chat files added

---
+++ README.md
@@ -0,0 +1,20 @@
+#Javascript training
+
+##Goal:
+Building a javascript application using Node.js as server and Express.js as framework.
+
+##Dependencies:
+
+###- Server -
+Node.js:    latest release
+Express.js:  latest release
+Socket.io:  latest release
+
+###- Frontend -
+Framework: to be chosen
+Template: to be chosen
+
+###- Database -
+MongoDB: latest release
+
+
---
+++ app/chat/bind_us_together.js
@@ -0,0 +1,19 @@
+(function () {
+    var express = require('express')();
+    var bindUsTogether = require('http').Server(express);
+    var io = require('socket.io')(bindUsTogether);
+
+    express.get('/', function (req, res) {
+        res.sendfile('index.html');
+    });
+
+    io.on('connection', function (socket) {
+        socket.on('chat message', function (msg) {
+            io.emit('chat message', msg);
+        });
+    });
+
+    bindUsTogether.listen(3000, function () {
+        console.log('Server starting on port %d', bindUsTogether.address().port);
+    });
+})();
